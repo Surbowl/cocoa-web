205f022ac389c64a298be53d5fda84d1c50fb739
surbowl <surbowl@gmail.com>
2020-10-21 16:54:50 +0800

    add min:css method and rebuild proj structure

--- src/CoreMe/Startup.cs
+++ src/CoreMe/Startup.cs
@@ -23,8 +23,10 @@ namespace CoreMe
             else
             {
                 app.UseExceptionHandler("/Home/Error");
+
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 //app.UseHsts();
+
                 //app.UseHttpsRedirection();
             }
 
--- src/CoreMe/gulpfile.js
+++ src/CoreMe/gulpfile.js
@@ -1,25 +1,32 @@
-﻿/// <binding BeforeBuild='auto' Clean='clean:js' />
-var gulp = require('gulp');
-var clean = require('gulp-clean');
-var browserify = require('browserify');
-var vueify = require('vueify')
-var envify = require('envify/custom')
-var source = require('vinyl-source-stream');
-var tsify = require('tsify');
-var uglify = require('gulp-uglify');
-var sourcemaps = require('gulp-sourcemaps');
-var buffer = require('vinyl-buffer');
+﻿/// <binding BeforeBuild='auto' Clean='clean' />
+const gulp = require('gulp');
+const clean = require('gulp-clean');
+const cleanCSS = require('gulp-clean-css');
+const rename = require('gulp-rename');
+const browserify = require('browserify');
+const vueify = require('vueify')
+const envify = require('envify/custom')
+const source = require('vinyl-source-stream');
+const tsify = require('tsify');
+const uglify = require('gulp-uglify');
+const sourcemaps = require('gulp-sourcemaps');
+const buffer = require('vinyl-buffer');
 
-// development or production
-const nodeEnv = 'production';
+const debug = false;
 
 const paths = {
     js: 'wwwroot/js/',
+    css: 'wwwroot/css/',
     lib: 'wwwroot/lib/'
 }
 
-// 需要编译的 ts 文件
+// 有 ts 文件需要编译的 Views
 const tsFiles = [
+    { controller: 'Home', action: 'Index' }
+]
+
+// 有 css 文件需要编译的 Views
+const cssFiles = [
     { controller: 'Home', action: 'Index' }
 ]
 
@@ -35,12 +42,6 @@ String.prototype.firstLowerCase = function () {
     });
 }
 
-// 清空 wwwroot/js 文件夹
-gulp.task('clean:js', function () {
-    return gulp.src(paths.js, { read: false, allowEmpty: true })
-        .pipe(clean());
-});
-
 // 将 lib 移动到 wwwroot/lib
 gulp.task('move:lib', done => {
     libs.forEach(function (item) {
@@ -50,34 +51,76 @@ gulp.task('move:lib', done => {
     done()
 });
 
-// 将 ts 编译为 min.js 并创建 map
-gulp.task('bundle:js', done => {
-    tsFiles.forEach(function (tsFile) {
-        browserify({
-            basedir: './Views',
-            entries: [tsFile.controller + '/' + tsFile.action + '.ts'],
-            cache: {},
-            packageCache: {}
-        })
-        .transform(vueify)
-        .transform(
-            // https://vuejs.org/v2/guide/deployment.html
-            { global: true },
-            envify({ NODE_ENV: nodeEnv })
-        )
-        .plugin(tsify)
-        .bundle()
-        .pipe(source(tsFile.action.firstLowerCase() + '.min.js'))
-        .pipe(buffer())
-        .pipe(sourcemaps.init({ loadMaps: true }))
-        .pipe(uglify())
-        .pipe(sourcemaps.write('./', {
-            mapFile: path => path.replace('.min.', '.')
-        }))
-        .pipe(gulp.dest(paths.js + tsFile.controller.firstLowerCase()));
+// 清空 wwwroot/js 文件夹
+gulp.task('clean:js', () => {
+    return gulp.src(paths.js, { read: false, allowEmpty: true })
+        .pipe(clean());
+});
+
+// 清空 wwwroot/css 文件夹
+gulp.task('clean:css', () => {
+    return gulp.src(paths.css, { read: false, allowEmpty: true })
+        .pipe(clean());
+});
+
+// 压缩 css 并创建 map
+gulp.task('min:css', done => {
+    cssFiles.forEach((cssFile) => {
+        gulp.src('./Views/' + cssFile.controller + '/' + cssFile.action + '.cshtml.css')
+            .pipe(sourcemaps.init())
+            .pipe(cleanCSS({ debug: debug }))
+            .pipe(rename(cssFile.action.firstLowerCase() + '.min.css'))
+            .pipe(sourcemaps.write('./', {
+                mapFile: path => path.replace('.min.', '.')
+            }))
+            .pipe(gulp.dest(paths.css + '/' + cssFile.controller.firstLowerCase()));
     });
     done();
 });
 
-// 执行所有任务
-gulp.task('auto', gulp.parallel(['move:lib', gulp.series(['clean:js', 'bundle:js'])]));
+// 将 ts 编译为 min.js 并创建 map
+gulp.task('min:js', done => {
+    tsFiles.forEach((tsFile) => {
+        // https://www.bookstack.cn/read/TypeScript-4.0-zh/8593981d0f4f8496.md
+        browserify({
+            basedir: './Views',
+            debug: debug,
+            entries: [tsFile.controller + '/' + tsFile.action + '.cshtml.ts'],
+            cache: {},
+            packageCache: {}
+        })
+            // https://vuejs.org/v2/guide/deployment.html
+            .transform(vueify)
+            .transform(
+                { global: true },
+                envify({ NODE_ENV: (debug ? 'development' : 'production') })
+            )
+            .plugin(tsify)
+            .bundle()
+            .pipe(source(tsFile.action.firstLowerCase() + '.min.js'))
+            .pipe(buffer())
+            .pipe(sourcemaps.init({ loadMaps: true }))
+            .pipe(uglify())
+            .pipe(sourcemaps.write('./', {
+                mapFile: path => path.replace('.min.', '.')
+            }))
+            .pipe(gulp.dest(paths.js + tsFile.controller.firstLowerCase()));
+    });
+    done();
+});
+
+// 清除生成的文件
+gulp.task('clean', gulp.parallel(['clean:css', 'clean:js']));
+
+// 清除生成的文件并重新编译
+gulp.task('auto', gulp.parallel([
+    'move:lib',
+    gulp.series([
+        'clean:css',
+        'min:css'
+    ]),
+    gulp.series([
+        'clean:js',
+        'min:js'
+    ])
+]));
