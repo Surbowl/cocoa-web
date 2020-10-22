87e2d9f6e17167920db3079382b170db34c794a6
Surbowl <Surbowl@gmail.com>
2020-10-22 12:39:05 +0800

    rebuild with https://github.com/danijelh/aspnetcore-vue-typescript-template

--- src/Startup.cs
+++ src/Startup.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Routing;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -9,19 +8,13 @@ namespace CoreMe
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddHttpClient();
             
             services.Configure<RouteOptions>(options => options.LowercaseUrls = true);
-            services.AddMvc();
+
+            services.AddControllersWithViews();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -29,6 +22,13 @@ namespace CoreMe
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                //app.UseHsts();
+
+                //app.UseHttpsRedirection();
             }
 
             app.UseStaticFiles();
