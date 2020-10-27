1cef30107fe9c6fed75282d299af49176558207f
Cai jiewen <bowl.bowl@qq.com>
2020-10-27 10:10:36 +0800

    add gulpfile for clean

---
+++ src/gulpfile.js
@@ -0,0 +1,13 @@
+﻿/// <binding Clean='clean' />
+const gulp = require('gulp');
+const rimraf = require('rimraf');
+
+const paths = {
+    css: './wwwroot/dist/css/',
+    js: './wwwroot/dist/js/'
+};
+
+gulp.task('clean-css', done => rimraf(paths.css, done));
+gulp.task('clean-js', done => rimraf(paths.js, done));
+
+gulp.task('clean', gulp.series(['clean-js', 'clean-css']));
--- src/webpack.config.js
+++ src/webpack.config.js
@@ -18,204 +18,204 @@ const applicationBasePath = "./VueApp/";
 var appEntryFiles = {}
 fs.readdirSync(applicationBasePath).forEach(function (name) {
 
-  let spaEntryPoint = applicationBasePath + name + '/app.ts'
+    let spaEntryPoint = applicationBasePath + name + '/app.ts'
 
-  if (fs.existsSync(spaEntryPoint)) {
-    appEntryFiles[name] = spaEntryPoint
-  }
+    if (fs.existsSync(spaEntryPoint)) {
+        appEntryFiles[name] = spaEntryPoint
+    }
 
-  spaEntryPoint = applicationBasePath + name + '/app.js'
-  if (fs.existsSync(spaEntryPoint)) {
-    appEntryFiles[name] = spaEntryPoint
-  }
+    spaEntryPoint = applicationBasePath + name + '/app.js'
+    if (fs.existsSync(spaEntryPoint)) {
+        appEntryFiles[name] = spaEntryPoint
+    }
 
 })
 
 // Add main site.scss file with Bulma(or any other source by choice)
 appEntryFiles["vendor"] = [
-  path.resolve(__dirname, "VueApp/common/design/site.scss"),
+    path.resolve(__dirname, "VueApp/common/design/site.scss"),
 ]
 
 module.exports = function (env, argv) {
 
-  if (argv.mode === "production") {
-    isProduction = true;
-  }
+    if (argv.mode === "production") {
+        isProduction = true;
+    }
 
-  return {
-    entry: appEntryFiles,
-    output: {
-      path: path.resolve(__dirname, "wwwroot/dist"),
-      filename: "js/[name]/bundle.js",
-      chunkFilename: "js/[name]/bundle.js?v=[chunkhash]",
-      publicPath: "/dist/"
-    },
-    resolve: {
-      extensions: [".ts", ".js", ".vue", ".json", "scss", "css"],
-      alias: {
-        vue$: "vue/dist/vue.esm.js",
-        "@": path.join(__dirname, applicationBasePath)
-      },
-      modules: [
-        path.join(__dirname, './node_modules')
-      ]
-    },
-    devtool: "source-map",
-    devServer: {
-      historyApiFallback: true,
-      noInfo: true,
-      overlay: true
-    },
-    module: {
-      rules: [
-        /* config.module.rule('vue') */
-        {
-          test: /\.vue$/,
-          loader: "vue-loader",
-          options: {
-            preserveWhitespace: false,
-            loaders: {
-              scss: "vue-style-loader!css-loader!sass-loader", // <style lang="scss">
-              sass: "vue-style-loader!css-loader!sass-loader?indentedSyntax" // <style lang="sass">
-            }
-          }
+    return {
+        entry: appEntryFiles,
+        output: {
+            path: path.resolve(__dirname, "wwwroot/dist"),
+            filename: "js/[name]/bundle.js",
+            chunkFilename: "js/[name]/bundle.js?v=[chunkhash]",
+            publicPath: "/dist/"
         },
-        /* config.module.rule('js') */
-        {
-          test: /\.js$/,
-          exclude: /(node_modules|bower_components)/,
-          loader: "babel-loader",
-          exclude: /node_modules/,
-        },
-        /* config.module.rule('ts') */
-        {
-          test: /\.ts$/,
-          loader: "ts-loader",
-          options: {
-            appendTsSuffixTo: [/\.vue$/],
-            transpileOnly: true
-          }
-        },
-        /* config.module.rule('sass') */
-        {
-          test: /\.scss$/,
-          use: [
-            {
-              loader: MiniCssExtractPlugin.loader,
-              options: {
-                publicPath: '/dist/',
-                // only enable hot in development
-                hmr: process.env.NODE_ENV === 'development',
-                // if hmr does not work, this is a forceful method.
-                reloadAll: true
-              },
+        resolve: {
+            extensions: [".ts", ".js", ".vue", ".json", "scss", "css"],
+            alias: {
+                vue$: "vue/dist/vue.esm.js",
+                "@": path.join(__dirname, applicationBasePath)
             },
-            'css-loader',
-            'postcss-loader',
-            'sass-loader',
-          ],
+            modules: [
+                path.join(__dirname, './node_modules')
+            ]
         },
-        /* config.module.rule('css') */
-        {
-          test: /\.css$/,
-          loader: "css-loader"
+        devtool: "source-map",
+        devServer: {
+            historyApiFallback: true,
+            noInfo: true,
+            overlay: true
         },
-        /* config.module.rule('images') */
-        {
-          test: /\.(png|jpe?g|gif|webp)(\?.*)?$/,
-          use: [
-            {
-              loader: 'url-loader',
-              options: {
-                limit: 4096,
-                fallback: {
-                  loader: 'file-loader',
-                  options: {
-                    name: 'img/[name].[hash:8].[ext]'
-                  }
+        module: {
+            rules: [
+                /* config.module.rule('vue') */
+                {
+                    test: /\.vue$/,
+                    loader: "vue-loader",
+                    options: {
+                        preserveWhitespace: false,
+                        loaders: {
+                            scss: "vue-style-loader!css-loader!sass-loader", // <style lang="scss">
+                            sass: "vue-style-loader!css-loader!sass-loader?indentedSyntax" // <style lang="sass">
+                        }
+                    }
+                },
+                /* config.module.rule('js') */
+                {
+                    test: /\.js$/,
+                    exclude: /(node_modules|bower_components)/,
+                    loader: "babel-loader",
+                    exclude: /node_modules/,
+                },
+                /* config.module.rule('ts') */
+                {
+                    test: /\.ts$/,
+                    loader: "ts-loader",
+                    options: {
+                        appendTsSuffixTo: [/\.vue$/],
+                        transpileOnly: true
+                    }
+                },
+                /* config.module.rule('sass') */
+                {
+                    test: /\.scss$/,
+                    use: [
+                        {
+                            loader: MiniCssExtractPlugin.loader,
+                            options: {
+                                publicPath: '/dist/',
+                                // only enable hot in development
+                                hmr: process.env.NODE_ENV === 'development',
+                                // if hmr does not work, this is a forceful method.
+                                reloadAll: true
+                            },
+                        },
+                        'css-loader',
+                        'postcss-loader',
+                        'sass-loader',
+                    ],
+                },
+                /* config.module.rule('css') */
+                {
+                    test: /\.css$/,
+                    loader: "css-loader"
+                },
+                /* config.module.rule('images') */
+                {
+                    test: /\.(png|jpe?g|gif|webp)(\?.*)?$/,
+                    use: [
+                        {
+                            loader: 'url-loader',
+                            options: {
+                                limit: 4096,
+                                fallback: {
+                                    loader: 'file-loader',
+                                    options: {
+                                        name: 'img/[name].[hash:8].[ext]'
+                                    }
+                                }
+                            }
+                        }
+                    ]
+                },
+                /* config.module.rule('svg') */
+                {
+                    test: /\.(svg)(\?.*)?$/,
+                    use: [
+                        {
+                            loader: 'file-loader',
+                            options: {
+                                name: 'img/[name].[hash:8].[ext]'
+                            }
+                        }
+                    ]
+                },
+                /* config.module.rule('media') */
+                {
+                    test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+                    use: [
+                        {
+                            loader: 'url-loader',
+                            options: {
+                                limit: 4096,
+                                fallback: {
+                                    loader: 'file-loader',
+                                    options: {
+                                        name: 'media/[name].[hash:8].[ext]'
+                                    }
+                                }
+                            }
+                        }
+                    ]
+                },
+                /* config.module.rule('fonts') */
+                {
+                    test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/i,
+                    use: [
+                        {
+                            loader: 'url-loader',
+                            options: {
+                                limit: 4096,
+                                fallback: {
+                                    loader: 'file-loader',
+                                    options: {
+                                        name: 'fonts/[name].[hash:8].[ext]'
+                                    }
+                                }
+                            }
+                        }
+                    ]
                 }
-              }
-            }
-          ]
+            ]
         },
-        /* config.module.rule('svg') */
-        {
-          test: /\.(svg)(\?.*)?$/,
-          use: [
-            {
-              loader: 'file-loader',
-              options: {
-                name: 'img/[name].[hash:8].[ext]'
-              }
-            }
-          ]
-        },
-        /* config.module.rule('media') */
-        {
-          test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
-          use: [
-            {
-              loader: 'url-loader',
-              options: {
-                limit: 4096,
-                fallback: {
-                  loader: 'file-loader',
-                  options: {
-                    name: 'media/[name].[hash:8].[ext]'
-                  }
+        plugins: [
+            new VueLoaderPlugin(),
+            new MiniCssExtractPlugin({
+                filename: "css/[name]/main.css"
+            }),
+            new webpack.DefinePlugin({
+                "process.env": {
+                    NODE_ENV: isProduction ? '"production"' : '""'
                 }
-              }
-            }
-          ]
-        },
-        /* config.module.rule('fonts') */
-        {
-          test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/i,
-          use: [
-            {
-              loader: 'url-loader',
-              options: {
-                limit: 4096,
-                fallback: {
-                  loader: 'file-loader',
-                  options: {
-                    name: 'fonts/[name].[hash:8].[ext]'
-                  }
+            }),
+            new webpack.ProvidePlugin({
+                Promise: "es6-promise-promise",
+                Vue: ["vue/dist/vue.esm.js", "default"]
+            }),
+            new webpack.HotModuleReplacementPlugin(),
+            new CompressionPlugin({
+                test: /\.js$|\.css$|\.html$/,
+                filename: "[path][base].gz[query]",
+                algorithm: "gzip"
+            }),
+            new RemovePlugin({
+                before: {
+                    // parameters for "before normal compilation" stage.
+                    include: [
+                        './wwwroot/dist'
+                    ]
                 }
-              }
-            }
-          ]
-        }
-      ]
-    },
-    plugins: [
-      new VueLoaderPlugin(),
-      new MiniCssExtractPlugin({
-        filename: "css/[name]/main.css"
-      }),
-      new webpack.DefinePlugin({
-        "process.env": {
-          NODE_ENV: isProduction ? '"production"' : '""'
-        }
-      }),
-      new webpack.ProvidePlugin({
-        Promise: "es6-promise-promise",
-        Vue: ["vue/dist/vue.esm.js", "default"]
-      }),
-      new webpack.HotModuleReplacementPlugin(),
-      new CompressionPlugin({
-        test: /\.js$|\.css$|\.html$/,
-        filename: "[path][base].gz[query]",
-        algorithm: "gzip"
-      }),
-      new RemovePlugin({
-        before: {
-          // parameters for "before normal compilation" stage.
-          include: [
-            './wwwroot/dist'
-          ]
-        }
-      })
-    ]
-  };
+            })
+        ]
+    };
 };
