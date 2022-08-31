b39e20d4c0cdcc71d39caece74b9b8fad78bd65d
Cai Jiewen <surbowl@gmail.com>
2022-08-31 09:04:38 +0800

    update beian.miit href

--- src/VueApp/home/en/App.vue
+++ src/VueApp/home/en/App.vue
@@ -149,7 +149,7 @@
                         <br />
                     </span>
                     <span>
-                        <a class="has-text-grey-dark is-size-7" href="http://www.beian.miit.gov.cn/">闽ICP备19009575号-1</a>
+                        <a class="has-text-grey-dark is-size-7" href="http://beian.miit.gov.cn/" target="_blank">京ICP备xxxxxxxx号-1</a>
                         <br />
                     </span>
                 </p>
